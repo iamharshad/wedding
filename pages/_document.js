f976eb76563b29c126ef17af22d2381d2336f7a0
iamharshad.prajapati@gmail.com <iamharshad.prajapati@gmail.com>
2021-12-05 00:58:05 +0530

    update og tag

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,7 @@ class MyDocument extends Document {
           <meta property="og:type" content="article" />
           <meta
             property="og:image"
-            content="https://wedding-aczpaausn-iamharshad.vercel.app/media/images/og-thumb.jpg"
+            content={`${window.location.origin}/media/images/og-thumb.jpg`}
           />
           <meta property="og:image:width" content="1280" />
           <meta property="og:image:height" content="640" />
