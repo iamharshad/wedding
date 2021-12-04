67074b9ac99a23ade852829dd454d5e341346e1b
iamharshad.prajapati@gmail.com <iamharshad.prajapati@gmail.com>
2021-12-04 23:42:04 +0530

    open graph image

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,7 @@ class MyDocument extends Document {
           <meta property="og:type" content="article" />
           <meta
             property="og:image"
-            content="https://wedding-pz8dfmkc3-iamharshad.vercel.app/media/images/groom.jpg"
+            content="https://wedding-pz8dfmkc3-iamharshad.vercel.app/media/images/og-thumb.jpg"
           />
           <meta property="og:image:width" content="1280" />
           <meta property="og:image:height" content="640" />
